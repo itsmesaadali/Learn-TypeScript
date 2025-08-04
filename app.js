ebac1632276a2cb5c6d36a4c2037b841f2440339
itsmesaadali <itmesaad@gmail.com>
2025-08-04 15:09:17 +0500

    start leaning typescript

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+// type safety
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 12;
+function hello() {
+    return 'hi';
+}
+let b = 'saad';
+let c = 'ali';
+//# sourceMappingURL=app.js.map
