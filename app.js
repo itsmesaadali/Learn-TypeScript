0538f1b09f065f54168a9767b4c5a974103b9038
itsmesaadali <itmesaad@gmail.com>
2025-08-05 14:58:30 +0500

    learn about types and annotations

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 "use strict";
-// type safety
+// Basic Types 
+// Primitive types (number, string, boolean)
+// Arrays
+// Tuples 
+// Enumes
+// any, unknown, void, null, undefined, Never
 Object.defineProperty(exports, "__esModule", { value: true });
-var a = 12;
-function hello() {
-    return 'hi';
+let arr = [1, 2, 3, 4,];
+let a = ['Saad', 21];
+let b;
+b = 12;
+b = 'Saad';
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super_admin";
+})(UserRoles || (UserRoles = {}));
+function abcd() {
+    console.log('Nothing');
 }
-let b = 'saad';
-let c = 'ali';
+function string() {
+    return 'Saad';
+}
+function number() {
+    return 12;
+}
+function bool() {
+    return true;
+}
+let c;
+c = 'Saad';
+c = null;
+let d;
+// function any():never{
+//     while(true){}
+// }
+// any()
+// console.log('hey')
+// types Inference 
+// Understanding type inference
+// Type annotations
+// automatically defined our tpye 
+// annotation mean tell which types 
+// let a:number
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,2 +1,62 @@
+// Basic Types 
+// Primitive types (number, string, boolean)
+// Arrays
+// Tuples 
+// Enumes
+// any, unknown, void, null, undefined, Never
+
 let arr: number[] = [1,2,3,4,];  
 let a:[string, number] = ['Saad', 21]
+
+let b;
+b = 12
+b = 'Saad'
+
+enum UserRoles{
+    ADMIN = 'admin',
+    GUEST = 'guest',
+    SUPER_ADMIN = 'super_admin'
+}
+
+function abcd():void {
+    console.log('Nothing')
+}
+
+function string():string {
+    return 'Saad'
+}
+
+
+function number():number {
+    return 12
+}
+
+function bool():boolean {
+    return true
+}
+
+let c:string | null
+
+c = 'Saad'
+c = null
+
+let d:undefined
+
+// function any():never{
+//     while(true){}
+// }
+
+// any()
+// console.log('hey')
+
+
+// types Inference 
+// Understanding type inference
+// Type annotations
+
+
+// automatically defined our tpye 
+
+// annotation mean tell which types 
+// let a:number
+
