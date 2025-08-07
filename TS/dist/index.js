7246571a5a4dcb4a7ce9aaddd554053f6b6e7f5d
itsmesaadali <itmesaad@gmail.com>
2025-08-07 21:36:57 +0500

    learn about classes & getter & setters

--- TS/dist/index.js
+++ TS/dist/index.js
@@ -1,4 +1,22 @@
-console.log('typescript is here still');
-console.log('typescript is amazing');
+// class User {
+//     public email:string
+//     private name:string
+//     private readonly city:string = 'ISB'
+//     constructor(email:string, name:string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+class User {
+    email;
+    name;
+    city = 'ISB';
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+}
+const saad = new User('itmesaad@gmail.com', 'Saad');
 export {};
+// saad.name 
 //# sourceMappingURL=index.js.map
--- TS/src/index.ts
+++ TS/src/index.ts
@@ -1,2 +1,44 @@
-console.log('typescript is here still');
-console.log('typescript is amazing');
+// class User {
+//     public email:string
+//     private name:string
+//     private readonly city:string = 'ISB'
+//     constructor(email:string, name:string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
+class User {
+
+    private _courseCount = 1
+
+    readonly city:string = 'ISB'
+    constructor(
+        public email:string, 
+        public name:string, 
+        // private userID:string
+    ) {}
+
+    private deleteToken() {
+        console.log('delete token')
+    }
+
+    get getAppleEmail():string {
+        return `apple${this.email}`
+    }
+
+    get courseCount():number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if(courseNum <=1 ){
+            throw new Error('Course count should be more than 1')
+        }
+        this._courseCount = courseNum
+    }
+}
+
+
+const saad = new User('itmesaad@gmail.com', 'Saad')
+// saad.name 
